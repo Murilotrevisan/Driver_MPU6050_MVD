cd76f7470947f806752a96d38b7add31e270fda4
Murilotrevisan <murilopasini@usp.br>
2021-11-17 16:31:35 -0300

    Função de leitura dos registradores

--- DRIVER_MPU6050/MPU6050_MVD.c
+++ DRIVER_MPU6050/MPU6050_MVD.c
@@ -83,18 +83,42 @@ error_t MPU6050_init(MPU6050_t mpu) {
 
 
 
-
-
-
 /* Leituras do sensor */
 
+error_t MPU6050_measure(MPU6050_t mpu, MPU6050_values_t *medida) {
 
+	//Realiza a leitura dos registradores dos resultados
+	//Separa os resultados em cada uma das leituras
+	//Converte para as medidas necessárias
+	//Armazena na struct de resultados
 
+	//Leitura de todos os registradores de resultados e save no buffer
+	error_t value_raw = i2c_readN(mpu.device, MPU_MEASURES, buffer_view);
 
+	//Separação das componentes da leitura
+	//Acelerômetro
+	uint16_t accel_x = (buffer[0] << 8) | buffer[1];
+	uint16_t accel_y = (buffer[2] << 8) | buffer[3];
+	uint16_t accel_z = (buffer[4] << 8) | buffer[5];
+	//Temperatura
+	uint16_t temp    = (buffer[6] << 8) | buffer[7];
+	//Giroscópio
+	uint16_t gyro_x  = (buffer[8] << 8) | buffer[9];
+	uint16_t gyro_y  = (buffer[10] << 8) | buffer[11];
+	uint16_t gyro_z  = (buffer[12] << 8) | buffer[13];
 
+	//Alocação na struct de resultados
+	//Acelerômetro
+	medida -> AccelX = (float)accel_x;
+	medida -> AccelY = (float)accel_y;
+	medida -> AccelZ = (float)accel_z;
+	//Temperatura
+	medida -> Temp   = (float)temp;
+	//Giroscópio
+	medida -> GyroX  = (float)gyro_x;
+	medida -> GyroY  = (float)gyro_y;
+	medida -> GyroZ  = (float)gyro_z;
 
-
-
-
-
+	return 0;
+}
 
